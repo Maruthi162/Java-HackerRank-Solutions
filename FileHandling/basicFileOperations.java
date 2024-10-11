2ac272dcca7d29f1ad7b3cb5c1591a8f03af3cca
Maruthi N <nmaruthi162@gmail.com>
2024-10-11 23:52:51 +0530

    Added file handling folder

---
+++ FileHandling/basicFileOperations.java
@@ -0,0 +1,31 @@
+package FileHandling;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+
+
+public class basicFileOperations {
+    public static void main(String[] args){
+        String content="Writting this piece of text using filewritter";
+        try(FileWriter fw= new FileWriter("test.txt", true)){ // opening file in append mode, by default file will be opened in overwrite mode which will delete the existing data
+            
+            fw.write(content);
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+
+        try(BufferedReader br= new BufferedReader(new FileReader("test.txt"))){
+            String line;
+            System.out.println("Reading test.txt file line by line: ");
+            while((line=br.readLine())!=null){
+                System.out.println(line);
+            }
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
