a3e64331395ae193e311415d7c18f306d45fc775
Maruthi N <nmaruthi162@gmail.com>
2024-10-28 17:03:39 +0530

    Added java stream Collect.groupingBy method

---
+++ Collections/StreamAPIMethods/GroupingBy.java
@@ -0,0 +1,15 @@
+package Collections.StreamAPIMethods;
+import java.util.*;
+import java.util.stream.*;
+public class GroupingBy {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Maruthi", "Govind", "Chandra", "Bhargavi", "Goodd");
+
+        // Group names by their length
+        Map<Integer, List<String>> namesGroupByCount = names.stream()
+            .collect(Collectors.groupingBy(String::length));
+
+        // Print the grouped names
+        System.out.println(namesGroupByCount);
+    }
+}
