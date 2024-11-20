eba3119a2344da83f17ea4fb1f06b75a98b38109
Maruthi N <nmaruthi162@gmail.com>
2024-11-21 00:17:39 +0530

    Thread Synchronization

---
+++ MultiThreading/Synchronization2.java
@@ -0,0 +1,68 @@
+package MultiThreading;
+
+class Counter {
+    int count;
+
+    // use synchronized to avoid collisions of two threads while accessing this at
+    // the same time
+    public synchronized void Increment() {
+        count++;
+    }
+}
+
+public class Synchronization2 {
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter c = new Counter();
+        // object for synchronization
+        final Object lock = new Object();
+
+        // thred t1 using anonymous class
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 1000; i++) {
+                    synchronized (lock) {
+                        c.Increment();
+                        lock.notify();
+                        try {
+                            if (i < 1000) {
+                                lock.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+
+        // Thread t2 using lambda expression
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 1000; i++) {
+                    synchronized (lock) {
+                        c.Increment();
+                        lock.notify();
+                        try {
+                            if (i < 1000) {
+                                lock.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join(); // used throws to avoid axceptions
+
+        System.out.println("Count = " + c.count);
+    }
+}
--- MultiThreading/ThreadPriority.java
+++ MultiThreading/ThreadPriority.java
@@ -2,6 +2,9 @@ package MultiThreading;
 
 public class ThreadPriority {
     public static void main(String[] args) {
+        // priority is represented in between 1 to 10
+        // 1 is least priority and
+        // 10 is max priority
         int a=30;
         int b=20;
         Thread t1= new Thread(()->{
@@ -16,9 +19,9 @@ public class ThreadPriority {
         System.out.println(t1.toString());
         System.out.println(t2.toString());
         //set min priority to Sum Thread
-        t1.setPriority(Thread.MIN_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY); //since t1 has min priority hence it will be executed after executing max priority threads
         //set max priority to Sub
-        t2.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY); //since t2 has max priority it will be executed first
          //print thread name and priority
          System.out.println(t1.toString());
          System.out.println(t2.toString());
